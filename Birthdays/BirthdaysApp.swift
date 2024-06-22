8a4e72139927c59609a55feec762444cf8fc2e7d
brozozowska <s_rozov@me.com>
2024-06-22 15:30:49 +0300

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Сергей Розов on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
